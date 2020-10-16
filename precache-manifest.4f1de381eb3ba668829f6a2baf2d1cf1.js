567da09953986bbd940acfde5ce9d2ea3f975873
Dawid Pniewski <dawidpniewski@gmail.com>
2020-10-16 13:33:40 +0200

    Updates

---
+++ precache-manifest.4f1de381eb3ba668829f6a2baf2d1cf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d7eb652445464967088",
+    "url": "/gitTest/static/css/main.d366660a.chunk.css"
+  },
+  {
+    "revision": "0d7eb652445464967088",
+    "url": "/gitTest/static/js/main.8d8cc824.chunk.js"
+  },
+  {
+    "revision": "7f0df81dbf4ef565a6c6",
+    "url": "/gitTest/static/js/runtime~main.71d88a12.js"
+  },
+  {
+    "revision": "9265742814159994725d",
+    "url": "/gitTest/static/js/2.3c5292f0.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/gitTest/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "410b563db0de60c376613d5a1a5ceaaa",
+    "url": "/gitTest/index.html"
+  }
+];
